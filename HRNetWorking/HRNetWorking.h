57f9b78bdd2566e77f07cae946379f6defa3c69a
iOSGeekOfChina <danny@cecelive.com>
2017-10-11 17:55:45 +0800

    创建

---
+++ HRNetWorking/HRNetWorking.h
@@ -0,0 +1,19 @@
+//
+//  HRNetWorking.h
+//  HRNetWorking
+//
+//  Created by iOSGeekOfChina on 2017/10/11.
+//  Copyright © 2017年 iOSGeekOfChina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HRNetWorking.
+FOUNDATION_EXPORT double HRNetWorkingVersionNumber;
+
+//! Project version string for HRNetWorking.
+FOUNDATION_EXPORT const unsigned char HRNetWorkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HRNetWorking/PublicHeader.h>
+
+
